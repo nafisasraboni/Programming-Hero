6db9bcbd321649dfbaddc13ca462c73b9f405fe5
nafisasraboni <nafisasraboni2207@gmail.com>
2026-07-15 00:52:09 +0600

    Create practiseTask.js

---
+++ Module_07/intro-to-js/practiseTask.js
@@ -0,0 +1,26 @@
+//1
+let drink="Coca-cola";
+const age=19;
+console.log(drink);
+console.log(age);
+
+//2
+console.log(typeof drink);
+console.log(typeof age);
+
+//3
+let ola="mew";
+let catSound="mew";
+console.log(catSound);
+
+//5
+let shoePrice = 2000;
+price = 3000;
+//console.log(price);
+
+price+=1000;
+console.log(price);
+price++;
+console.log(price);
+console.log(price++);
+console.log(++price);
