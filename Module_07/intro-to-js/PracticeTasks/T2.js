61670426845a02da1dfd3c0a250958c3e3854da3
nafisasraboni <nafisasraboni2207@gmail.com>
2026-07-15 01:13:03 +0600

    Complete Task2

---
+++ Module_07/intro-to-js/PracticeTasks/T2.js
@@ -0,0 +1,13 @@
+let num1=25;
+let num2=10;
+let sum= num1+num2;
+let sub= num1-num2;
+let mul= num1*num2;
+let div= num1/num2;
+let mod= num1%num2;
+
+console.log("Addition:"+ sum);
+console.log("Subtration:"+ sub);
+console.log("Multiplication:"+ mul);
+console.log("Division:"+ div);
+console.log("Modulus:"+ mod);
