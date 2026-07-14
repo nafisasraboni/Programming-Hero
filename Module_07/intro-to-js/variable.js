f8d55b4287fc30549b73dbba4dc5c102ef8b664a
nafisasraboni <nafisasraboni2207@gmail.com>
2026-07-14 22:29:11 +0600

    learn variable in js

---
+++ Module_07/intro-to-js/variable.js
@@ -0,0 +1,22 @@
+//variable
+var weight =38;
+var temperature = 38;
+var people = 38;
+//console.log(people);
+
+//let,const 
+let price=100;
+
+const pI=200;
+
+let chocolate="Dairy_Milk";
+let money=100000000;
+let place="Switzerland";
+let food="Biriyani";
+let drink="MOJO";
+
+console.log(chocolate);
+console.log(money);
+console.log(place);
+console.log(food);
+console.log(drink);
