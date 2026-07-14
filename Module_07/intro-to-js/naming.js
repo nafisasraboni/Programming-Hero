a786a3971044b57561dfc1d57d91b15ee1b4b97b
nafisasraboni <nafisasraboni2207@gmail.com>
2026-07-14 23:27:45 +0600

    learned variable declaring rules

---
+++ Module_07/intro-to-js/naming.js
@@ -0,0 +1,33 @@
+let location = 'BBAn';
+let age = 18;
+let boyos = 19;
+
+//Rules
+//1. Variable name should be something meaningful.It will indicate what it contains.
+let tagadhumdhumtagadhumdhumnaaaaaaa = 97;
+let avc = 45;
+let infoo = 'mewwww';
+let brb = 'Jasmu';
+
+//do not use shorthand.
+//It's okay if the name is longer.
+
+//2.Cannot use a keyword
+const let=43;
+const false=false;
+
+//3. Has to be in one word.can not contain any space or gap
+const i am sraboni=32424;
+const i_am_sraboni=32424; //snack case
+const iAmSraboni=32424;   //Camel case (usually used)
+const IAmSraboni=32424;   //pascal case
+
+//4. name cannot start with a number,but number can be used anywhere other than the first letter.
+const 3person=3; //not right
+const person3=6; //right
+const p3erson=7; //right
+
+//5.other than letter name can start with either _ or $.
+//cannot use any other spacial character
+
+//6. variable is case sensitive.
