85c2028865d40bd486ac387f6bf41849f3e6b97d
nafisasraboni <nafisasraboni2207@gmail.com>
2026-07-15 00:20:00 +0600

    Shortcut learned

---
+++ Module_07/intro-to-js/shorthand.js
@@ -0,0 +1,23 @@
+let price=700;
+//console.log(price);
+
+price = 600;
+//console.log(price);
+
+// price=price-50;
+// console.log(price);
+
+//shortcut
+price-=50;
+console.log(price);
+
+price+=50;
+console.log(price);
+
+price++;
+price--;
+++price;
+
+console.log(price++);
+console.log(price);
+console.log(++price);
