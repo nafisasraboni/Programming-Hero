dda3c6cdea9b404b87317a753e3ac413da0f19e3
nafisasraboni <nafisasraboni2207@gmail.com>
2026-07-15 21:29:26 +0600

    learn && and || logical operators

---
+++ Module_08/Conditionals/multiple.js
@@ -0,0 +1,34 @@
+/* Multiple conditions */
+
+const salary = 750000;
+const isBCS = false;
+const hasCar = false;
+
+// if(salary>50000){
+//     console.log("Su......patro")
+// }else{
+//     console.log("Tomar kopale biye nai!!!!!!!")
+// }
+
+
+
+// && 
+// if(salary>50000 && isBCS === true && hasCar === true){
+//     console.log("Su......patro")
+// }else{
+//     console.log("Tomar kopale biye nai!!!!!!!")
+// }
+
+
+
+// ||
+if(salary>5000000 || isBCS === true || hasCar === true){
+    console.log("Su......patro")
+}else{
+    console.log("Tomar kopale biye nai!!!!!!!")
+}
+
+
+
+
+
