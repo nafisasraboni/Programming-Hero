29d2d6e0353b9ad6b7b79a701439c14dc0924d37
nafisasraboni <nafisasraboni2207@gmail.com>
2026-07-15 23:52:41 +0600

    Added questions

--- Module_08/Conditionals/Practice-Tasks/t2.js
+++ Module_08/Conditionals/Practice-Tasks/t2.js
@@ -1,3 +1,5 @@
+//Guess the result of "5" == 5 and "5" === 5 first, then write code to verify your guess.
+
 if("5"==5){
     console.log("Loose Equal")
 }
--- Module_08/Conditionals/Practice-Tasks/t3.js
+++ Module_08/Conditionals/Practice-Tasks/t3.js
@@ -1,3 +1,6 @@
+//Create a variable isRaining (true/false). If it is true, print "Take an umbrella".
+
+
 const isRaining = true;
 if(isRaining){
     console.log("Take an umbrella")
