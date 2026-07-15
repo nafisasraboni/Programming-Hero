d35f2b7f23092446a465087342de63715b5617ff
nafisasraboni <nafisasraboni2207@gmail.com>
2026-07-15 20:47:42 +0600

    condition er structure learn : if only

---
+++ Module_08/Conditionals/if.js
@@ -0,0 +1,38 @@
+/* Conditionals 
+
+------------------------------------
+            Structure
+------------------------------------
+if(condition){
+
+}
+*/
+
+// if(true){
+//     console.log("I am dead")
+// }
+
+// if(false){
+//     console.log("You're dead")
+// }
+
+// if(8>4){
+//     console.log("step inside the loop")
+// }
+
+// const price = 10000;
+// if(price<3000000){
+//     console.log("I need to make more money")
+// }
+
+// const price = 10000;
+// const money = 900;
+// if(price<money){
+//     console.log("I need to make more money")
+// }
+
+const name="Aluuu";
+if(name==="aluuu"){
+    console.log(name)
+}
+
