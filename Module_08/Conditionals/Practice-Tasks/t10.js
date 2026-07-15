e4c01c90aa0523eba27debb20c65fa433097d104
nafisasraboni <nafisasraboni2207@gmail.com>
2026-07-16 00:14:00 +0600

    complete task 10

---
+++ Module_08/Conditionals/Practice-Tasks/t10.js
@@ -0,0 +1,10 @@
+// Build a simple login system — print "Login successful" only if both username and password are correct.
+
+let username="Nafisa";
+let password=1234;
+
+if(username==="Moon" && password===1234){
+    console.log("Login successful")
+}else{
+    console.log("Try again!!")
+}
