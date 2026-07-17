e95a4ac101bc34fd5bf74f3b9f4cc0a2391e2c94
nafisasraboni <nafisasraboni2207@gmail.com>
2026-07-17 22:47:12 +0600

    practice for loop

---
+++ Module_09/decreasing.js
@@ -0,0 +1,9 @@
+// for(let i=100;i>=50;i-=4){
+//     console.log("Number: ",i)
+// }
+
+console.log("Start count downnnn!!")
+for(let i=5;i>0;i-=1){
+    console.log(i);
+}
+console.log("BOOOOMMMMMMMMMM!!!!");
