4de0ffa2714859a9796ad2f59eb613d7a4690c13
nafisasraboni <nafisasraboni2207@gmail.com>
2026-07-17 21:09:51 +0600

    learn the concept of for-loop

---
+++ Module_09/loop.js
@@ -0,0 +1,21 @@
+/* For Loop, We need:
+1. starting situation
+2. count value adapt
+3. stopping condition 
+   (until ends or fixed number of times)
+4. repeatitive same or similar task
+*/
+
+// let count = 0;
+// count<10;
+// count++;
+// console.log("Output")
+
+//for-loop structue
+// for(Initialization; condition; modification){
+//     repeatitive task;
+// }
+
+for(let count=0; count<1000; count++){
+    console.log("Mew!!")
+}
---
+++ Module_09/repeat.js
@@ -0,0 +1,11 @@
+//in general repeated way
+console.log("Show me something")
+console.log("Show me something")
+console.log("Show me something")
+console.log("Show me something")
+console.log("Show me something")
+console.log("Show me something")
+console.log("Show me something")
+console.log("Show me something")
+console.log("Show me something")
+console.log("Show me something")
