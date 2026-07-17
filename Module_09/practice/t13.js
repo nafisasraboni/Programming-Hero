3b0349065d4631f53861cb06eed81877d4b691a3
nafisasraboni <nafisasraboni2207@gmail.com>
2026-07-18 02:19:05 +0600

    practice for,while and do-while loop for one ques

---
+++ Module_09/practice/t13.js
@@ -0,0 +1,20 @@
+//for loop
+for(let i=1;i<=5;i++){
+    console.log("Num:",i);
+}
+
+//while loop
+let i=1;
+
+while(i<=5){
+    console.log("Num:",i);
+    i++;
+}
+
+//do-while loop
+let i=1;
+
+do{
+    console.log("Num:",i);
+    i++;
+}while(i<=5)
