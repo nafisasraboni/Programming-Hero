ac0ca4ad0e9e0addbc05b33fd7c8021f40fc4fab
nafisasraboni <nafisasraboni2207@gmail.com>
2026-07-19 02:51:07 +0600

    Learn array traversal using for and while loop

---
+++ Module_10/for.js
@@ -0,0 +1,6 @@
+const numbers = [78,89,90,91,92,93,94,95];
+
+for(let i=0;i<numbers.length;i++){
+    console.log("Index: ",i)
+    console.log("Value: ",numbers[i])
+}
---
+++ Module_10/loop.js
@@ -0,0 +1,5 @@
+const numbers = [78,89,90,91,92,93,94,95];
+
+for(const num of numbers){
+    console.log(num)
+}  
