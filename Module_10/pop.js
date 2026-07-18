32fe16b524777341ac10a89d638ad4a7f32c4174
nafisasraboni <nafisasraboni2207@gmail.com>
2026-07-19 01:42:38 +0600

    learn push & pop

---
+++ Module_10/pop.js
@@ -0,0 +1,6 @@
+let money = ["Rupee","Pound","Dollar"];
+const firstOut=money.pop();
+console.log(money);
+const secondOut=money.pop();
+console.log(money);
+console.log(firstOut,secondOut);
---
+++ Module_10/push.js
@@ -0,0 +1,7 @@
+const friends = ["sami", "ali", "reza"];
+friends.push("mona");
+console.log(friends); 
+
+const destinations=['London','Italy','Japan'];
+destinations.push("Switzerland");
+console.log(destinations)
