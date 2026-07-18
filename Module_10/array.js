440df7714be5df23e9c214c59cd7cbe50c45bf87
nafisasraboni <nafisasraboni2207@gmail.com>
2026-07-19 00:31:28 +0600

    array introduction learn

---
+++ Module_10/array.js
@@ -0,0 +1,15 @@
+//Variable can hold only one value or data
+
+const player1 = 40 ;
+const player2 = 42 ;
+const player3 = 45 ;
+const player4 = 76 ;
+const player5 = 84 ;
+const player6 = 64 ;
+const player7 = 49 ;
+
+//array can hold more than one value
+const team =[40,42,45,76,84,64,49];
+const playerName = ["ina", "mina", "dina", "bina", "pina", "dekka", "porsi"];
+const isRich = [true,true,true,true,false,true,false]
+
