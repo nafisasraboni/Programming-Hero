db36f5bfe91f614e42973167c3f1d53a8f3068f3
nafisasraboni <nafisasraboni2207@gmail.com>
2026-07-23 10:53:57 +0600

    learn basic string

---
+++ Module_11/string.js
@@ -0,0 +1,10 @@
+const cup = "World Cup";
+const tea = "Green Tea";
+const location = `Bandorban`;
+// const vehicle = new String('car');
+
+console.log(cup.length);
+console.log(cup[7]);
+
+location[3]='g';
+console.log(location);
