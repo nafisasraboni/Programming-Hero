0e745aeef44cc7ad4556c7a30377f40f8374d3dd
nafisasraboni <nafisasraboni2207@gmail.com>
2026-07-25 23:36:57 +0600

    Count Vowels in a String

---
+++ Module_13/Problem_6.js
@@ -0,0 +1,25 @@
+// Problem 6: Count Vowels in a String
+
+let string = "i am not a kid";
+// let vowel = ['a', 'e', 'i', 'o', 'u'];
+
+// for (let s of string) {
+//         if (s === 'a'||s === 'e'||s === 'i'||s === 'o'||s === 'u') {
+//             console.log("Vowel");
+//         } else {
+//             console.log("no vowel")
+//         }
+// }
+
+function checkVowel(string) {
+    for (let s of string) {
+        if (s === 'a' || s === 'e' || s === 'i' || s === 'o' || s === 'u') {
+            console.log("Vowel");
+        } else if(s === " "){
+            console.log("Gap");
+        }else{
+            console.log("Consonent")
+        }
+    }
+}
+checkVowel(string)
