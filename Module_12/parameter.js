75c212f460f8e88b4c0a39961da09018718dfbb3
nafisasraboni <nafisasraboni2207@gmail.com>
2026-07-25 16:46:41 +0600

    learn about parameter

---
+++ Module_12/parameter.js
@@ -0,0 +1,29 @@
+// function sayHello(name){
+//     console.log("Hello!",name);
+// }
+
+// sayHello("Nafisa");
+
+//subtraction
+function sub(num1,num2){
+    console.log("Given numbers are: ",num1,",",num2);
+    const subb = num1-num2;
+    console.log("Substraction of these numbers = ",subb);
+}
+sub(1110984747, 65967988);
+
+//multiplication
+function mul(num1,num2,num3,num4){
+    console.log("Given numbers are: ",num1,",",num2,",",num3,",",num4);
+    const mull = num1*num2*num3*num4;
+    console.log("Multiplication of these numbers = ",mull);
+}
+mul(11,65,7,89);
+
+//square
+function squ(num){
+    console.log("Given number is: ",num);
+    const square = num*num;
+    console.log("square of the number = ",square);
+}
+squ(11);
