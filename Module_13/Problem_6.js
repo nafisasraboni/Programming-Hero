c4ab466189279e100aa696fb304e3dc69b1eb2ce
nafisasraboni <nafisasraboni2207@gmail.com>
2026-07-25 23:41:27 +0600

    add vowel count

--- Module_13/Problem_6.js
+++ Module_13/Problem_6.js
@@ -1,6 +1,7 @@
 // Problem 6: Count Vowels in a String
 
 let string = "i am not a kid";
+let vowelCount=[];
 // let vowel = ['a', 'e', 'i', 'o', 'u'];
 
 // for (let s of string) {
@@ -15,11 +16,15 @@ function checkVowel(string) {
     for (let s of string) {
         if (s === 'a' || s === 'e' || s === 'i' || s === 'o' || s === 'u') {
             console.log("Vowel");
+            vowelCount.push(s);
+
         } else if(s === " "){
             console.log("Gap");
         }else{
             console.log("Consonent")
         }
     }
+    return vowelCount.length;
 }
-checkVowel(string)
+const count=checkVowel(string)
+console.log("Total number of vowel:",count)
