9882a6f7f9242dd1075401f9c17e1834f1f97b8d
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-04 20:50:16 +0600

    learn Truthy and Falsy values

---
+++ Module_17/Truthy_Falsy.js
@@ -0,0 +1,18 @@
+if(false){
+    console.log("Output asbe na falsy value er jonno")
+}
+if(0){
+    console.log("Output asbe na falsy value er jonno")
+}
+if(undefined){
+    console.log("Output asbe na falsy value er jonno")
+}
+if(""){
+    console.log("Output asbe na falsy value er jonno")
+}
+if(null){
+    console.log("Output asbe na falsy value er jonno")
+}
+if(NaN){
+    console.log("Output asbe na falsy value er jonno")
+}
