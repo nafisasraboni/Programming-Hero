72b58115a8238300ce03819352387b35b1b3d685
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-10 02:22:40 +0600

    Update callback.js

--- Module_17/callback.js
+++ Module_17/callback.js
@@ -13,12 +13,43 @@
 
 // console.log(studentInfo(myInfo))
 
-function calculator(a,b,cb){
-    let sum = a+b;
-    cb(sum);
-}
-function totalResult(result){
-    console.log(result)
+// function calculator(a,b,cb){
+//     let sum = a+b;
+//     cb(sum);
+// }
+// function totalResult(result){
+//     console.log(result)
+// }
+
+// calculator(5,5,totalResult);
+
+
+
+function registerStudent(cb){
+    console.log("User is registering. Need more data");
+
+    cb();
+    return cb();
 }
 
-calculator(5,5,totalResult);
+function userBasicInfo(){
+    let student={
+        name:"Moon",
+        age:26,
+        town:"Raj"
+    }
+    return student;
+}
+console.log(registerStudent(userBasicInfo));
+
+
+
+function fruitPrice(cb){
+    cb();
+    return cb();
+}
+function total(mango,apple,banana){
+    let sum = 250+676+453;
+    return sum;
+}
+console.log(fruitPrice(total))
