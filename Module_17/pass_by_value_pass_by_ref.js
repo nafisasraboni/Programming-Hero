bc0b32c44a8b0c27adb91257d3c808de5537e132
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-10 02:48:45 +0600

    learn  Pass by value and pass by reference

--- Module_17/pass_by_value_pass_by_ref.js
+++ Module_17/pass_by_value_pass_by_ref.js
@@ -1,9 +1,20 @@
-let name = "Nafisa";
+// pass by value
 
-function myName(myname){
-    myname = "Sraboni"
-    return(myname)
+let name = "Payel";
+
+function myfunc1(myname){
+    myname = "Bristy";
+    return myname;
 }
-console.log(myName(name))
+console.log(myfunc1());
+console.log(name);
 
-//console.log(name)
+//pass by reference 
+
+let studentMarks = [210,450,567];
+function eachMarks(marks){
+    marks.push(780);
+    return marks;
+}
+console.log(eachMarks(studentMarks));
+console.log(studentMarks);
