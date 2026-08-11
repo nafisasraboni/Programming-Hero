b9fbc539c8322cc38520a7ad63dd8a168d366d30
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-12 02:37:59 +0600

    Update t10.js

--- Module_18/practice_tasks/t10.js
+++ Module_18/practice_tasks/t10.js
@@ -1,4 +1,4 @@
-/* Update) Return a new array with the value at index replaced by newValue, without mutating the original array.
+/* Immutable Array Update) Return a new array with the value at index replaced by newValue, without mutating the original array.
 Input:  cloneAndUpdate([5, 10, 15], 1, 99)
 Output: [5, 99, 15]
 (original array still [5, 10, 15])
