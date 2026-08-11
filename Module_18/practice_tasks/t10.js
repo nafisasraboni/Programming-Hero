6106b24c622ea972ec1ed1a050f51679e9d8ae41
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-12 02:37:23 +0600

    Spread (Immutable Array  Update)
    Return a new array with the value at index replaced by newValue, without mutating the original array.
    Input:  cloneAndUpdate([5, 10, 15], 1, 99)
    Output: [5, 99, 15]
    (original array still [5, 10, 15])

---
+++ Module_18/practice_tasks/t10.js
@@ -0,0 +1,14 @@
+/* Update) Return a new array with the value at index replaced by newValue, without mutating the original array.
+Input:  cloneAndUpdate([5, 10, 15], 1, 99)
+Output: [5, 99, 15]
+(original array still [5, 10, 15])
+ */
+
+function cloneAndUpdate(arr, index, newValue){
+    let newarr=[...arr];
+    newarr[index]=newValue;
+    return newarr;
+}
+let arr = [5, 10, 15]
+console.log( cloneAndUpdate(arr, 1, 99))
+console.log(arr)
