ded962ed9ac8ddef73ef10ddef60fa21f6405cf1
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-13 05:17:00 +0600

    Task 9: isStrictMatch(input, target) — Truthy/Falsy and ===
    Return true only if input strictly equals target in both value and type, false otherwise.
    Input: isStrictMatch("18", 18)
    Output: false
    Input: isStrictMatch(18, 18)
    Output: true

---
+++ Module_19/practice/9.js
@@ -0,0 +1,16 @@
+/* Task 9: isStrictMatch(input, target) — Truthy/Falsy and === 
+Return true only if input strictly equals target in both value and type, false otherwise.
+Input: isStrictMatch("18", 18) 
+Output: false
+Input: isStrictMatch(18, 18) 
+Output: true
+ */
+
+const isStrictMatch=(input, target)=>{
+    if(input===target){
+        return true
+    }else{
+        return false
+    }
+}
+console.log(isStrictMatch(18, 18))
