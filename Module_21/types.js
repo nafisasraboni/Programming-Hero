75a0e9cebd922d25a20c570a129515c0e834e077
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-15 03:33:30 +0600

    learn primitive data type in array

---
+++ Module_21/types.js
@@ -0,0 +1,9 @@
+"use strict";
+let person = "Nafisa";
+person = "Sharmin";
+console.log(person);
+let isMarried = true;
+console.log(isMarried);
+let nums = [1, 2, 3, 4, 5];
+nums = ["one", "two", "three", "four", "five"];
+console.log(nums);
---
+++ Module_21/types.ts
@@ -0,0 +1,12 @@
+let person : string = "Nafisa"
+person="Sharmin"
+
+console.log(person)
+
+let isMarried : boolean = true
+console.log(isMarried)
+
+let nums : any = [1,2,3,4,5]
+nums = ["one","two","three","four","five"]
+console.log(nums)
+
