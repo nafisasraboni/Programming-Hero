9bee7e0839d01f014505a50959503f1754bdfa50
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-15 03:07:43 +0600

    Learn the basic about typeScript and tsc process( both manually and automatic)

---
+++ Module_21/first.js
@@ -0,0 +1,6 @@
+let products="Sunglass"
+products = 456
+products = true
+products = ["book","pen","pencil"]
+
+console.log(products)
---
+++ Module_21/items.js
@@ -0,0 +1,6 @@
+"use strict";
+let items = "laptop";
+// items = 100;
+let numbers = [23, 45, 78];
+console.log(items);
+console.log(numbers);
