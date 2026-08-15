61b4441721406c77fc985a65c321e46e886cb5b6
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-15 21:45:34 +0600

    Problem Statement:3
    একটি array বানাও যেখানে student-এর নাম থাকবে এবং কতজন আছে সেটা কনসোল করবে।
    Input:
    ["Amina", "Rahim", "Karim", "Salma", "Rafi"]
    Output:
    Total student: 5

---
+++ Module_21/practice/3.js
@@ -0,0 +1,5 @@
+"use strict";
+function studentCount(student) {
+    return student.length;
+}
+console.log(studentCount(["Amina", "Rahim", "Karim", "Salma", "Rafi"]));
---
+++ Module_21/practice/3.ts
@@ -0,0 +1,4 @@
+function studentCount(student:string[]):number{
+    return student.length;
+}
+console.log(studentCount(["Amina", "Rahim", "Karim", "Salma", "Rafi"]))
