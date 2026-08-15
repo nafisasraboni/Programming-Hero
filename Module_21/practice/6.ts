c0b2df05d6591aa887244cfb2103506e1b7b7050
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-16 01:18:45 +0600

    Problem Statement: 6
    একটি function লেখো যা একটি number নিয়ে check করবে সেটা even কি না।
    Input:
    isEven(8)
    Output:
    true

---
+++ Module_21/practice/6.ts
@@ -0,0 +1,15 @@
+/* একটি function লেখো যা একটি number নিয়ে check করবে সেটা even কি না।
+Input:
+isEven(8)
+Output:
+true
+ */
+
+function isEven(num:number):boolean{
+    if(num%2===0){
+        return true
+    }else{
+        return false
+    }
+}
+console.log(isEven(8))
