a9e5c92ca38f39ce57c643bb5eb1fd88a1c3832c
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-15 14:27:17 +0600

    learn map in ts

---
+++ Module_21/map.js
@@ -0,0 +1,5 @@
+"use strict";
+const marks = [25, 76, 90, 56, 67];
+const mappedMarks = marks.map(mark => {
+    console.log(mark * 2);
+});
---
+++ Module_21/map.ts
@@ -0,0 +1,5 @@
+const marks :number[] = [25,76,90,56,67]
+
+const mappedMarks : number[] = marks.map(mark=>{
+    return(mark*2)
+})
