88d99b42b88dd2a39e030a13af323b22e7362573
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-15 13:38:25 +0600

    learn literals and optional properties

---
+++ Module_21/recap.ts
@@ -0,0 +1,27 @@
+//optional property
+
+const studento: {
+    name: string,
+    age: number,
+    isAdult?: boolean
+} = {
+    name: "Nafisa",
+    age: 23,
+    //isAdult: true
+}
+
+//literals
+
+const studentoo: {
+    name: "Nafisa",
+    age: number,
+    isAdult?: boolean
+} = {
+    name: "Nafisa",
+    age: 23,
+    //isAdult: true
+}
+
+studentoo.name = "Payel"
+
+
---
+++ Module_21/tuples.js
@@ -0,0 +1,4 @@
+"use strict";
+const location2 = [23.56, 45];
+const entries = ["age", 29];
+const foodPrice = ["pizza", 500];
