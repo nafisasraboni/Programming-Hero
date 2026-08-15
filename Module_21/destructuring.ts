0b0064b7b7957f27298b0ad6c16b88940a43904b
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-15 14:40:00 +0600

    learn destructuring

---
+++ Module_21/destructuring.ts
@@ -0,0 +1,12 @@
+//object 
+const user :{name:string,id:number} = {
+    name:"Nafisa",
+    id:223311064
+}
+
+const {name:userName,id:UserId} = user
+
+//array
+ 
+const userInfo = ["Sraboni",23]
+const[userInfoName,age]=userInfo
