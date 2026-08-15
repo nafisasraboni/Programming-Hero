7196a68fd4c66e6736fde41345c5f7664350aa47
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-15 14:00:19 +0600

    learn Functions in TypeScript, typing inputs and outputs

---
+++ Module_21/function.js
@@ -0,0 +1,11 @@
+"use strict";
+// function add(num1, num2){
+//     const sum = num1+num2;
+//     return sum
+// }
+// console.log(add(6,7))
+function add(num1, num2) {
+    const sum = num1 + num2;
+    return sum;
+}
+console.log(add(6, 7));
---
+++ Module_21/function.ts
@@ -0,0 +1,16 @@
+// function add(num1, num2){
+//     const sum = num1+num2;
+//     return sum
+// }
+// console.log(add(6,7))
+
+function add(num1:number, num2:number):number{
+    const sum = num1+num2;
+    return sum
+}
+
+console.log(add(6,7))
+
+function studious ( nums:number[],location:string):boolean{
+    return false;
+}
