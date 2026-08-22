92f6ff04ced4c55b67630d9677818f72c34e2b0e
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-22 19:19:00 +0600

    Create class.ts

---
+++ Module_23/src/class.ts
@@ -0,0 +1,21 @@
+/*
+name
+email
+age
+marks
+*/   
+
+class Student{
+    name:string = "Sraboni"
+    email:string ="nafisa@gmail.com"
+    age:number = 23
+    marks:number = 587
+}
+
+//instantiate
+//instance
+
+const sra = new Student();
+console.log(sra)
+console.log()
+console.log(sra)
