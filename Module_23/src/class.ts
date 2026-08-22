85c0132966ff23ee7cfc28fd9ea951da01df6f62
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-22 23:16:58 +0600

    learn class,object and constructor

--- Module_23/src/class.ts
+++ Module_23/src/class.ts
@@ -6,16 +6,25 @@ marks
 */   
 
 class Student{
-    name:string = "Sraboni"
-    email:string ="nafisa@gmail.com"
-    age:number = 23
-    marks:number = 587
+    name:string 
+    email:string 
+    age:number 
+    marks:number 
+
+    // constructor ---> special method
+    constructor(name:string, email:string ,age:number,marks:number){
+        this.name=name;
+        this.email=email;
+        this.age=age;
+        this.marks=marks;
+    }
 }
 
 //instantiate
 //instance
 
-const sra = new Student();
+const sra = new Student("Sraboni","s@gmail.com",23,100);
+const payel = new Student("Poo","p@gmail.com",31,110);
+
 console.log(sra)
-console.log()
-console.log(sra)
+console.log(payel)
