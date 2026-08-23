e6b8ef5fcb73a3d2316a6929385ebbcbcb7af0f5
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-23 22:00:03 +0600

    Complete the problem's solution

--- Module_24/src/p4.ts
+++ Module_24/src/p4.ts
@@ -1,12 +1,19 @@
-interface Product{
-    name:string,
-    price:number
+interface Product {
+    name: string,
+    price: number
 }
+const calculateCartTotal = (product: Product[]): number => {
+    let total = product.reduce((accumulation, currentvalue) => {
+        accumulation += currentvalue.price
+        return accumulation
+    }, 0)
+    return total
 
-
-const calculateCartTotal=(product:Product[]):number=>{
-    let eachProduct = product.map(item=>{
-        let total = 0;
-        total
-    })
 }
+const product = [
+    { name: "Keyboard", price: 1500 },
+    { name: "Mouse", price: 800 },
+    { name: "USB Cable", price: 300 }
+];
+
+console.log(calculateCartTotal(product));
