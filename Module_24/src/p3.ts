e20a239f8e7493cbc29c3000d911db6b78b1e88c
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-24 02:45:54 +0600

    Update p3.ts

--- Module_24/src/p3.ts
+++ Module_24/src/p3.ts
@@ -1,10 +1,8 @@
-type User = {
+function formatUserProfile(user: {
     name: string,
     age: number,
     city: string
-}
-
-function formatUserProfile(user: User): string {
+}): string {
     return `${user.name} is ${user.age} years old and lives in ${user.city}`
 }
 
