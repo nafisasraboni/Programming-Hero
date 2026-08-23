aded6a95bb20600c88a0dd8b305a5d6be4db1a49
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-23 23:58:13 +0600

    Problem 6 — Role-Based Permission Checker
    Function name must be: canEdit
    Scenario
    A web application has three types of users: admin, editor, and viewer. Different roles have different permissions. For this problem, only administrators and editors are allowed to edit content.
    Task
    First create a union type Role, then create a function named canEdit that receives a valid Role and returns whether that role can edit content.
    Rules
    Role
    Can Edit?
    admin
    true
    editor
    true
    viewer
    false
    
    TypeScript Requirement
    This should produce a TypeScript error — canEdit("guest") — since the purpose is to make TypeScript restrict the function to known roles.
    Starter Code
    
    function canEdit(role: Role): <type> {
    
        // write your code here
    
    }
    Example
    canEdit("admin");
    // true
    
    canEdit("editor");
    // true
    
    canEdit("viewer");
    // false

---
+++ Module_24/src/p6.ts
@@ -0,0 +1,13 @@
+type Role = "admin"|"editor"|"viewer"
+
+function canEdit(role:Role):boolean{
+    if(role==="admin" || role==="editor"){
+        return true
+    }else{
+        return false
+    }
+}
+console.log(canEdit("admin"));
+console.log(canEdit("editor"));
+console.log(canEdit("viewer"));
+console.log(canEdit("guest"));
