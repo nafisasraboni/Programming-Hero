7f92efc63682f0c76a119c5e557a07443574f88a
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-30 21:32:14 +0600

    learn first class function and it's rules

--- Advance_JS/pure_function_first_class_high_order.js
+++ Advance_JS/pure_function_first_class_high_order.js
@@ -7,22 +7,57 @@
 
 
 // pure function
-function myFunc (a,b){
-    return a+b;
+// function myFunc (a,b){
+//     return a+b;
+// }
+// console.log(myFunc(5,6))
+// console.log(myFunc(5,6))
+// console.log(myFunc(5,6))
+// console.log(myFunc(5,6))
+
+// //impure function => change hocche
+// let sum=0;
+// function myFunction (num){
+//     return sum+=num
+// }
+// console.log(myFunction(8))
+// console.log(myFunction(8))
+// console.log(myFunction(8))
+// console.log(myFunction(8))
+
+
+// first class function rules:
+// 1. A function can be stored as a variable
+// 2. A function can be stored as an array
+// 3. A function can be stored as an object
+// 4. We can create a function as we need
+
+//first class function rule 1:
+let func = function(a,b){
+    return a+b
 }
-console.log(myFunc(5,6))
-console.log(myFunc(5,6))
-console.log(myFunc(5,6))
-console.log(myFunc(5,6))
-
-//impure function => change hocche
-let sum=0;
-function myFunction (num){
-    return sum+=num
+console.log(func(4,5))
+//or,
+function funct(a,b){
+    return a+b
 }
-console.log(myFunction(8))
-console.log(myFunction(8))
-console.log(myFunction(8))
-console.log(myFunction(8))
+let functi= funct;
+console.log(functi(4,5))
 
+//first class function rule 2:
+let arr=[];
+arr.push(func)
+console.log(arr[0](100,300))
 
+//first class function rule 3:
+let myObj ={function : func} // or {func} es6 addition
+console.log(myObj.function(650,765))
+
+//first class function rule 4:
+function outerFunc(){
+    function innerFunc(){
+        console.log("Insidious")
+    }
+    innerFunc();
+}
+outerFunc();
