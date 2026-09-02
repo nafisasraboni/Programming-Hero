8fd7831a822d6f1be0f7e32efb03e99829fe1c62
nafisasraboni <nafisasraboni2207@gmail.com>
2026-09-03 03:25:02 +0600

    Apply css using className in react

--- Module_27/my-react-app/src/App.tsx
+++ Module_27/my-react-app/src/App.tsx
@@ -3,30 +3,41 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Get started, Already</h1>
-      <Person></Person>
-      <Gagets></Gagets>
+      <h1>React</h1>
+      {/* <Person></Person>
+      <Gagets></Gagets> */}
+      <Student></Student>
+      <Student></Student>
+      <Student></Student>
     </>
   );
 }
 
-function Person() {
-    return <p>I am here</p>
-}
+// function Person() {
+//     return <p>I am here</p>
+// }
 
-function Gagets(){
-  let name = "Payel"
-  return (
-    <>
-    <p>I am sraboni. My age is {25-2}</p>
-    <p>I am sraboni</p>
-    <p>I am not sraboni. I am {name}</p>
-    <p>I am sraboni</p>
-    <p>I am sraboni</p>
-    </>
+// function Gagets(){
+//   let name = "Payel"
+//   return (
+//     <>
+//     <p>I am sraboni. My age is {25-2}</p>
+//     <p>I am sraboni</p>
+//     <p>I am not sraboni. I am {name}</p>
+//     <p>I am sraboni</p>
+//     <p>I am sraboni</p>
+//     </>
+//   )
+// }
+
+function Student(){
+  return(
+    <div className="Student">
+    <h3>Name:</h3>
+    <p>Grade</p>
+  </div>
   )
 }
-
 
 
 export default App;
