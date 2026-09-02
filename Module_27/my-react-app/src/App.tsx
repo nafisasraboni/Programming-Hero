56878ccbaf5bdef1f25c65ee82c4b0ac27686024
nafisasraboni <nafisasraboni2207@gmail.com>
2026-09-03 03:50:03 +0600

    Practice the learnings till now

--- Module_27/my-react-app/src/App.tsx
+++ Module_27/my-react-app/src/App.tsx
@@ -6,9 +6,11 @@ function App() {
       <h1>React</h1>
       {/* <Person></Person>
       <Gagets></Gagets> */}
+      {/* <Student></Student>
       <Student></Student>
-      <Student></Student>
-      <Student></Student>
+      <Student></Student> */}
+      <Foodname></Foodname>
+      <Mathio></Mathio>
     </>
   );
 }
@@ -30,25 +32,59 @@ function App() {
 //   )
 // }
 
-function Student(){
+// function Student(){
 
-  // const studentStyle = {
-  //   border:'2px solid red',
-  //   borderRadius:'10px',
-  //   margin:'10px'
-  // }
-  
+//   // const studentStyle = {
+//   //   border:'2px solid red',
+//   //   borderRadius:'10px',
+//   //   margin:'10px'
+//   // }
+
+//   return(
+//     <div style={{
+//     border:'2px solid red',
+//     borderRadius:'10px',
+//     margin:'10px'
+//   }}>
+//     <h3>Name:</h3>
+//     <p>Grade</p>
+//   </div>
+//   )
+// }
+
+
+
+//practice
+
+function Mathio(){
+  const name="Moon"
   return(
-    <div style={{
-    border:'2px solid red',
-    borderRadius:'10px',
-    margin:'10px'
-  }}>
-    <h3>Name:</h3>
-    <p>Grade</p>
-  </div>
+    <>
+    <p>this is {name}</p>
+    <p>this is {name}'s age : {35-10+1}</p>
+    
+    </>
   )
 }
 
+function Foodname(){
+  // const stylingFood={
+  //   border: '2px solid black',
+  //   borderRadius: '5px',
+  //   margin: '10px'
+  // }
+  return(
+    <div style={{
+    border: '2px solid black',
+    borderRadius: '5px',
+    margin: '10px'
+  }}>
+      <h2>Pasta</h2>
+      <h2>Pizza</h2>
+      <h2>Pulao</h2>
+      <h2>Biriyani</h2>
+    </div>
+  )
+}
 
 export default App;
