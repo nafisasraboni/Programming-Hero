89c527a94260d2214d99977625cec3ee167cd789
nafisasraboni <nafisasraboni2207@gmail.com>
2026-09-05 04:39:14 +0600

    Learn event and different event handling ways through onClick

--- Module_28/my-event-app/src/App.tsx
+++ Module_28/my-event-app/src/App.tsx
@@ -1,11 +1,29 @@
-import './App.css'
+import "./App.css";
 
 function App() {
+  // function handleClick() {
+  //   alert("Button clicked");
+  // }
+
+  const handleClick = ()=>{
+    alert("Click me before I run,Hehe!!");
+  }
+
+  const handleAddToCart=(id)=>{
+    alert("Buy this "+ id)
+  }
+
   return (
     <>
-      
+      {/* <button onClick={handleClick}>Click me</button> */}
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={()=>{
+        alert("click quickly!!")
+      }}>Click me 2</button>
+
+      <button onClick={() =>handleAddToCart(65)}>Click me 3</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
