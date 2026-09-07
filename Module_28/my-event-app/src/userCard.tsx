a03a4cf6bcef02070df60c9245ad1cb9be48ef23
nafisasraboni <nafisasraboni2207@gmail.com>
2026-09-07 19:19:53 +0600

    Add css to the userCard

--- Module_28/my-event-app/src/userCard.tsx
+++ Module_28/my-event-app/src/userCard.tsx
@@ -1,6 +1,8 @@
+import './userCard.css';
+
 export default function UserCard({user}){
     return(
-        <div>
+        <div className="user">
             <h3>Name: {user.name}</h3>
             <h3>Id: {user.id}</h3>
         </div>
