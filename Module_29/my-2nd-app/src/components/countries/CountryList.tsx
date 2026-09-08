605e126041140071628a5653cf0ab31c07291dd7
nafisasraboni <nafisasraboni2207@gmail.com>
2026-09-08 13:27:46 +0600

    Display some more informations and a button

--- Module_29/my-2nd-app/src/components/countries/CountryList.tsx
+++ Module_29/my-2nd-app/src/components/countries/CountryList.tsx
@@ -1,23 +1,24 @@
-import  { use } from "react"
+import { use } from "react";
 //import CountryCard from "./CountryCard";
 import type { CountryType } from "../type";
 import Country from "../country/country";
+import "./CountryList.css";
 
 export interface CountryListProps {
-    countries: Promise<CountryType[]>
+  countries: Promise<CountryType[]>;
 }
 
 export default function CountryList({ countries }: CountryListProps) {
-    const countryNames = use(countries);
-    console.log(countryNames)
-    return(
-        <div>
-            <h2>Country List</h2>
-            {
-                countryNames.map(country=><Country key={country.ccn3.ccn3} country={country}></Country>)
-            }
-        </div>
-    )
+  const countryNames = use(countries);
+  console.log(countryNames);
+  return (
+    <div>
+      <h2>Country List</h2>
+      <div className="countryList">
+        {countryNames.map((country) => (
+          <Country key={country.ccn3.ccn3} country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 }
-
-
--- Module_29/my-2nd-app/src/components/country/country.tsx
+++ Module_29/my-2nd-app/src/components/country/country.tsx
@@ -10,7 +10,13 @@ export default function Country({ country }: CountryProps) {
     return(
         <div className="country">
             <h4>Official : {country.name.official}</h4>
+            <h5>Capital : {country.capital.capital}</h5>
             <h5>Population : {country.population.population}</h5>
+            <h5>languages : {country.languages.languages.eng}</h5>
+            <h5>languages : {country.languages.languages.jam}</h5>
+            <h5>ccn3 : {country.ccn3.ccn3}</h5>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <button>Visited</button>
         </div>
     ) 
 }
