1ea2f1f42d82ee5a16ce99e7762130c4730fc38f
nafisasraboni <nafisasraboni2207@gmail.com>
2026-09-10 01:06:18 +0600

    Update country.tsx

--- Module_29/my-2nd-app/src/components/country/country.tsx
+++ Module_29/my-2nd-app/src/components/country/country.tsx
@@ -1,22 +1,28 @@
-import type { CountryType } from "../type"
-import './country.css'
+import { useState } from "react";
+import type { CountryType } from "../type";
+import "./country.css";
 
 export interface CountryProps {
-    country: CountryType
+  country: CountryType;
 }
 
 export default function Country({ country }: CountryProps) {
-    
-    return(
-        <div className="country">
-            <h4>Official : {country.name.official}</h4>
-            <h5>Capital : {country.capital.capital}</h5>
-            <h5>Population : {country.population.population}</h5>
-            <h5>languages : {country.languages.languages.eng}</h5>
-            <h5>languages : {country.languages.languages.jam}</h5>
-            <h5>ccn3 : {country.ccn3.ccn3}</h5>
-            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
-            <button>Visited</button>
-        </div>
-    ) 
+
+    con
+  const handleVisited = () => {
+    console.log("I have visited");
+  };
+
+  return (
+    <div className="country">
+      <h4>Official : {country.name.official}</h4>
+      <h5>Capital : {country.capital.capital}</h5>
+      <h5>Population : {country.population.population}</h5>
+      <h5>languages : {country.languages.languages.eng}</h5>
+      <h5>languages : {country.languages.languages.jam}</h5>
+      <h5>ccn3 : {country.ccn3.ccn3}</h5>
+      <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+      <button onClick={handleVisited}>Visited</button>
+    </div>
+  );
 }
